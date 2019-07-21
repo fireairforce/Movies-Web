d7af63ab0e334e10c76fe13495a24021ab026e79
fireairforce <1344492820@qq.com>
2019-07-21 18:35:48 +0800

    feat:完成了电影数据库模型的建立

---
+++ server/database/schema/movie.js
@@ -0,0 +1,50 @@
+// 电影的数据模型
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+// Mixed比较适用于数据类型变化比较频繁的数据
+const Mixed = Schema.Types.Mixed;
+
+const MovieSchema = new Schema({
+    doubanId: String,
+    rate: Number,
+    title: String,
+    // 简介
+    summary: String,
+    // url地址
+    video: String,
+    // 海报图
+    poster: String,
+    // 封面地址
+    cover: String,
+
+    // 图床上面的ID
+    videoKey: String,
+    posterKey: String,
+    coverKey: String,
+    
+    rawTitle: String,
+    // 电影的类别,申明成数组，数组里面的每一个值都为字符串
+    movieTypes: [String],
+
+    pubdate: Mixed,
+    year: Number,
+    // 标签使用数组类型声明数组
+    tags: Array,
+
+    meta:{
+        // 这条数据被创建时的时间
+        createdAt: {
+            type: Date,
+            default: Date.now()
+        },
+        // 这条数据上传的时间
+        upDatedAt: {
+            type: Date,
+            default: Date.now()
+        }
+    }
+});
+// 先将这个模型建立起来，然后再将这个模型发布出去
+// 传入两个参数
+mongoose.model('Movie',MovieSchema)
